3caf20c7422d496ad8280572aa61fadf3e0ce132
Felix Brousseau-Tremblay <felix.brousseau-tremblay.1@live.com>
2025-07-30 21:15:32 +0200

    Simplify to the minimum

--- src/coursier/install.sh
+++ src/coursier/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install dependencies
-if ! type apt-get > /dev/null 2>&1; then
+if ! type apt-get >/dev/null 2>&1; then
     echo "This feature requires apt-get to be available."
     exit 1
 fi
@@ -12,27 +12,19 @@ apt-get install -y --no-install-recommends curl gzip ca-certificates
 # Determine architecture
 ARCHITECTURE="$(uname -m)"
 case ${ARCHITECTURE} in
-    x86_64) CS_ARCH="x86_64-pc-linux";;
-    aarch64 | arm64) CS_ARCH="aarch64-pc-linux";;
-    *)
-        echo "(!) Architecture ${ARCHITECTURE} not supported."
-        exit 1
-        ;;
+x86_64) CS_ARCH="x86_64-pc-linux" ;;
+aarch64 | arm64) CS_ARCH="aarch64-pc-linux" ;;
+*)
+    echo "(!) Architecture ${ARCHITECTURE} not supported."
+    exit 1
+    ;;
 esac
 
 # Download and install coursier
 DOWNLOAD_URL="https://github.com/coursier/launchers/raw/master/cs-${CS_ARCH}.gz"
 echo "Downloading Coursier launcher from ${DOWNLOAD_URL}"
-curl -sSL --fail "${DOWNLOAD_URL}" | gzip -d > /usr/local/bin/cs
+curl -sSL --fail "${DOWNLOAD_URL}" | gzip -d >/usr/local/bin/cs
 chmod +x /usr/local/bin/cs
-
-# Create .bash_profile if it doesn't exist, so cs setup will add the PATH
-touch ~/.bash_profile
 
 # Run setup
 cs setup --yes
-
-# Source .bash_profile to make the PATH available to the current shell
-# and add it to .bashrc for future non-login shells.
-echo '. ~/.bash_profile' >> ~/.bashrc
-. ~/.bash_profile
